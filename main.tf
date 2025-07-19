8ef4d75912cef9911afcccaeb1d43bfc032dba22
ARPIT SAGAR <98328210+ARPIT226@users.noreply.github.com>
2025-07-19 13:40:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,13 +2,6 @@ provider "aws" {
   region = var.region
 }
 
-data "aws_availability_zones" "available" {
-  filter {
-    name   = "opt-in-status"
-    values = ["opt-in-not-required"]
-  }
-}
-
 locals {
   cluster_name = "my-custom-cluster-226"
 }
